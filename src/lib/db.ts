c08f44ee8d7cb38de951bef9128d30e6d52f32e3
jph-sw <jph.sw@proton.me>
2025-03-28 16:09:43 +0000

    update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -35,3 +35,19 @@ pool.query(`
     user_id TEXT NOT NULL
   )
   `);
+
+pool.query(
+  `create table if not exists "user" ("id" text not null primary key, "name" text not null, "email" text not null unique, "emailVerified" boolean not null, "image" text, "createdAt" timestamp not null, "updatedAt" timestamp not null);`,
+);
+
+pool.query(
+  `create table if not exists "session" ("id" text not null primary key, "expiresAt" timestamp not null, "token" text not null unique, "createdAt" timestamp not null, "updatedAt" timestamp not null, "ipAddress" text, "userAgent" text, "userId" text not null references "user" ("id"));`,
+);
+
+pool.query(
+  `create table if not exists "account" ("id" text not null primary key, "accountId" text not null, "providerId" text not null, "userId" text not null references "user" ("id"), "accessToken" text, "refreshToken" text, "idToken" text, "accessTokenExpiresAt" timestamp, "refreshTokenExpiresAt" timestamp, "scope" text, "password" text, "createdAt" timestamp not null, "updatedAt" timestamp not null);`,
+);
+
+pool.query(
+  `create table if not exists "verification" ("id" text not null primary key, "identifier" text not null, "value" text not null, "expiresAt" timestamp not null, "createdAt" timestamp, "updatedAt" timestamp);`,
+);
