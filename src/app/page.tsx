cf944449559ee1d1d1977f4b789431bb619b03ab
jph-sw <jph-sw@proton.me>
2025-04-03 13:07:36 +0000

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
 import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { data: organizations } = authClient.useListOrganizations();
+
+  console.log(organizations);
   return redirect("/home");
 }
